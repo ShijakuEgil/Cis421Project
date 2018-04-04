6b7a86d33498401a0caa8f145b95dee69cf61631
Rabih Salamey <rysalame@umich.edu>
2018-04-04 15:17:11 -0400

    query: add in some sql functions [1/2]

--- functionsDb/db_query.php
+++ functionsDb/db_query.php
@@ -5,11 +5,38 @@
 DATABASE QUERIES ARE DONE HERE
 */
 
+//bring in the connection header.
+include("db_connect.php");
 /*
 functionality Login Page: 1 Validate Login
 */
-function validate_login($email, $password){
-  $password = sha1($email . $password);
+function validate_login($email, $password, $usertype){
+  global $connection;
+
+  $shapassword = sha1($email . $password);
+  if($usertype == "admin") {
+    $sql = "SELECT CASE WHEN EXISTS (
+  	         SELECT *
+  	         FROM [Employee]
+  	         WHERE employeeID = {$email}
+  	         AND password = {$shapassword}
+           )
+           THEN CAST(1 AS BIT)	//returns true if user exists and password is valid
+           ELSE CAST(0 AS BIT) END	//fail user";
+
+  } else {
+
+  $sql = "SELECT CASE WHEN EXISTS (
+	         SELECT *
+	         FROM [Students]
+	         WHERE employeeID = {$email}
+	         AND password = {$shapassword}
+         )
+         THEN CAST(1 AS BIT)	//returns true if user exists and password is valid
+         ELSE CAST(0 AS BIT) END	//fail user";
+   }
+  $result = $connection->query($sql);
+  //return $result  //will be true or false.
   return true;
 }
 
@@ -50,14 +77,26 @@ function search_for_books($book_name, $book_id, $author){
   $found_name = "Carl the magician";
   $found_id = 123456789;
   $author = "Michael Scott";
+
+  global $connection;
+  $sql = "SELECT * FROM Book
+          WHERE LIKE %{$book_name}%
+          AND LIKE %{$book_id}%
+          AND LIKE %{$author}% ";
+  $result = $connection->query($sql);
+  //return $result;
   return array($found_name, $found_id, $author);
 }
 
 /*
 functionality Student Page: 3 Account View
 */
-function account_info(){
+function account_info($studentid){
+  global $connection;
+  $sql = "SELECT * FROM Student
+          WHERE studentid = {$studentid}"
   return "Student info here";
+  $result = $connection->query($sql);
 }
 
 /*
@@ -72,6 +111,12 @@ functionality Admin Page: 1 add new user
 */
 function add_new_user($f_name, $l_name, $address, $email_address, $password){
   $added_user = true;
+  $shapassword = sha1($password);
+  global $connection;
+  $sql = "INSERT INTO student(first_name, last_name, address, email, password)
+          VALUES ({$f_name}, {$l_name}, {$address}, {$email_address}, {$shapassword})"
+  $result = $connection->query($sql);
+  //return $result; //will tell you what # of what columns are charged
   return $added_user;
 }
 /*
@@ -84,7 +129,12 @@ function returns(){
 functionality Admin Page: 3 Employee Data tab
 */
 function employee_info(){
-  $employee_info = array(//stub array
+  global $connection;
+  $sql = "SELECT * FROM Employee";
+  $result = $connection->query($sql);
+
+
+/*  $employee_info = array(//stub array
     array(
       'fname' => 'Mark',
       'lname' => 'Otto',
@@ -109,8 +159,8 @@ function employee_info(){
       'address' =>  '1111 address  Adress, AD 1111',
       'isStudent' => 'Yes'
     ),
-  );
-  return $employee_info;// this will be the array with all the table rows
+  );*/
+  return $result;// this will be the array with all the table rows
 }
 
 /*
@@ -133,6 +183,16 @@ function remove_books($book_title, $isbn, $qty){
 functionality Admin Page: 5 outstanding rentals
 */
 function outstanding_rentals(){
+  global $connection;
+  $currtime = date('Y-m-d H:i:s', strtotime($value['current_date']));
+
+  $sql = "SELECT * FROM Book
+          WHERE IN(
+          	SELECT * FROM Student
+          	WHERE date_due < {$currtime}
+        	)";  //need sql table/ERD updates
+  $result = $connection->query($sql);
+
   $late_rentals = array(//stub array
     array(
       'fname' => 'Mark',
@@ -153,5 +213,6 @@ function outstanding_rentals(){
       'days_late' =>  '4'
     ),
   );
+  //return $result;
   return $late_rentals;//array that holds all rows with late rentals
 }
